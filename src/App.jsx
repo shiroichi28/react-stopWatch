b0a853835c2a02e5653cbc4071400e8cb69d3bd8
shiroichi28 <manigandans28@gmail.com>
2023-06-15 22:59:35 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [time, setTime] = useState(0);
-  const [running, setRunning] = useState(false);
-
-  useEffect(() => {
-    let startTime = 0;
-    let rafId;
-
-    const updateTimer = (timestamp) => {
-      if (!startTime) {
-        startTime = timestamp;
-      }
-      const elapsedTime = timestamp - startTime;
-      setTime(elapsedTime);
-
-      rafId = requestAnimationFrame(updateTimer);
-    };
-
-    if (running) {
-      rafId = requestAnimationFrame(updateTimer);
-    }
-
-    return () => cancelAnimationFrame(rafId);
-  }, [running]);
-
-  const formatTime = (milliseconds) => {
-    const minutes = Math.floor(milliseconds / 60000);
-    const seconds = Math.floor((milliseconds % 60000) / 1000);
-    const hundredths = Math.floor((milliseconds % 1000) / 10);
+    const [time, setTime] = useState(0)
+    const [running, setRunning] = useState(false)
+    useEffect(() => {
+        let interval
+        if (running) {
+            interval = setInterval(() => {
+                setTime((prevTime) => prevTime + 10);
+            }, 10);
+        } else if (!running) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval)
+    }, [running]);
 
     return (
-      ("0" + minutes).slice(-2) +
-      ":" +
-      ("0" + seconds).slice(-2) +
-      ":" +
-      ("0" + hundredths).slice(-2)
+        <>
+            <h1 className="uppercase">Stopwatch</h1>
+            <div className="text-3xl font-bold m-5">
+                <span>{("0" + Math.floor(time / 60000) % 60).slice(-2)}:</span>
+                <span>{("0" + Math.floor(time / 1000) % 60).slice(-2)}:</span>
+                <span>{("0" + (time / 10) % 100).slice(-2)}</span>
+            </div>
+            <div className="flex items-center justify-between">
+                {running ? (<button  onClick={() => { setRunning(false) }}>Stop</button>
+                ) : (<button onClick={() => { setRunning(true) }}>Start</button>
+                )}
+                <button onClick={() => { setTime(0) }}>Reset</button>
+            </div>
+        </>
     );
-  };
-
-  const handleStartStop = () => {
-    setRunning((prevRunning) => !prevRunning);
-  };
-
-  const handleReset = () => {
-    setRunning(false);
-    setTime(0);
-  };
-
-  return (
-    <>
-      <h1 className="uppercase">Stopwatch</h1>
-      <div className="text-3xl font-bold m-5">{formatTime(time)}</div>
-      <div className="flex items-center justify-between">
-        <button onClick={handleStartStop}>{running ? "Stop" : "Start"}</button>
-        <button onClick={handleReset}>Reset</button>
-      </div>
-    </>
-  );
 }
 
 export default App;
